b451cbe499d891b0f9b0f1a36c69022721301da6
Robert Silverberg <rhs31@miami.edu>
2019-03-23 18:00:26 -0400

    Added resume and website functionality

---
+++ Assets/PortfolioScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortfolioScript : MonoBehaviour
+{
+    
+    public void OpenWebsite()
+    {
+        Application.OpenURL("https://rhs31.github.io/");
+    }
+}
